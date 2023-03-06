b06748cf7c4d872541a86805c1a6c18901b480b3
Petr Havlát <havlat82@gmail.com>
2023-03-07 00:46:44 +0100

    Added new class Pen to folder "Console" in project Havlat82.BugabooLibrary.ConsoleDebugApp with 2 basic overloads of System.Console.Write() methods

---
+++ sandbox/Havlat82.BugabooLibrary.ConsoleDebugApp/Console/Pen.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Havlat82.BugabooLibrary.ConsoleDebugApp.Console
+{
+    internal class Pen
+    {
+    }
+}
